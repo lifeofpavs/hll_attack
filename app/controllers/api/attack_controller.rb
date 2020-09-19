fcd39181c269b393b365d118a21e768da0c124c4
scipio13 <adell13pablo@gmail.com>
2020-09-19 14:50:01 +0200

    Updating v4

--- app/controllers/api/attack_controller.rb
+++ app/controllers/api/attack_controller.rb
@@ -2,17 +2,28 @@ module Api
   class AttackController < ApplicationController
       
     def initialize(attack_length, currentCardinality)
-      puts currentCardinality
       @expectedCardinality = attack_length.to_i
       @currentCardinality = currentCardinality.to_i
-      @oldCardinality 
+      @oldCardinality = 0 
       @attack_vector = []
     end
   
     def reset
       #Reset conversions table 
       Conversion.delete_all
+      ConversionFase2.delete_all
+      ConversionFiltered.delete_all
+      AttackVector.delete_all
+      AttackVectorFase2.delete_all
+      AttackVectorFiltered.delete_all
+      ActiveRecord::Base.connection.execute("Truncate table conversions ")
+      ActiveRecord::Base.connection.execute("Truncate table conversion_fase2s")
+      ActiveRecord::Base.connection.execute("Truncate table conversion_filtereds ")
+      ActiveRecord::Base.connection.execute("Truncate table attack_vectors ")
+      ActiveRecord::Base.connection.execute("Truncate table attack_vector_fase2s ")
+      ActiveRecord::Base.connection.execute("Truncate table attack_vector_filtereds ")
       ActiveRecord::Base.connection.execute("Truncate table summary_test ")
+      ActiveRecord::Base.connection.execute("Truncate table summary_conversions ")
     end
   
     def fase1 #Fase 1 
@@ -23,7 +34,7 @@ module Api
       #Populate and check cardinality
 
       #Keep populating until desired cardinality is achieved
-      for i in 89773...@expectedCardinality do 
+      for i in 0...@expectedCardinality do 
         oldCardinality = @currentCardinality #Get cardinality before insertion
 
         puts "ID: #{i}"
@@ -41,12 +52,10 @@ module Api
       diff = finish - start
 
       Utils::Slack::HllBot.send_message("Finished simulation at #{finish} with *#{@expectedCardinality}* ", "simulations")
-      Utils::Slack::HllBot.send_message("Took #{diff} seconds to complete. Total size of attack_vector is #{@attack_vector.size}", "simulations")
+      Utils::Slack::HllBot.send_message("Took #{diff} seconds to complete. Total size of attack_vector is #{AttackVector.all.count}", "simulations")
       Execution.create(time: diff.to_s, vector_size: AttackVector.all.count)
-      Utils::Slack::HllBot.send_message("Cardinality for #{@expectedCardinality} elements is #{Utils::PrestoDb.new.get_cardinality}", "simulations")
-      Utils::Slack::HllBot.send_message("Cardinality for Attack Vector with #{AttackVector.all.count} elements is #{Utils::PrestoDb.new.get_cardinality_attack_vector}", "simulations")
-      error = (Utils::PrestoDb.new.get_cardinality - Utils::PrestoDb.new.get_cardinality_attack_vector).to_i
-      Utils::Slack::HllBot.send_message("Error is #{error} of #{(error * 100)/@expectedCardinality.to_i}%", "simulations")
+      Utils::Slack::HllBot.send_message("Cardinality for #{@expectedCardinality} elements is #{@currentCardinality}", "simulations")
+      Utils::Slack::HllBot.send_message("Cardinality for Attack Vector with #{AttackVector.all.count} elements is #{Utils::PrestoDb.new.get_cardinality_attack_vector("attack_vectors")}", "simulations")
       
     end
 
@@ -54,18 +63,19 @@ module Api
       #Crear una nueva coleccion de AttackVecgor --> AttackVectorFase2 
       #Get elements not in db 
       start = Time.now
+      Utils::Slack::HllBot.send_message("Starting simulation fase 2  at #{start} with *#{@expectedCardinality}* ", "simulations")
+
       actual_attack_vector = AttackVector.all.pluck(:number).uniq #Get uniq elements in attack vector
-      conversions = [*55578..@expectedCardinality] #Populate array with elements from 1 to 100k
+      conversions = [*0..@expectedCardinality] #Populate array with elements from 1 to 100k
 
       #New HLL | Resetear el summary_test
       ActiveRecord::Base.connection.execute("Truncate table summary_test ")
       #Meter elementos en vector de ataque primero
       AttackVector.all.each do |vector_item|
         ConversionFase2.create(conversion_id: vector_item.number, conversion_date: "2020-03-19", user_id: 1)
+        AttackVectorFase2.create(number: vector_item.number)
       end
 
-      AttackVectorFase2.all.each do |item|
-      end
       Utils::PrestoDb.new.insert_all_element_presto_fase2
       puts "Done injecting fase2 attack vector"
       #Sacar cardinalidad 
@@ -77,7 +87,7 @@ module Api
       conversions_2 = conversions.select { |e| !(actual_attack_vector.include? (e))} #Perform except in order to get elements not in attack vector 
       puts "Done getting elements not in array. Length #{conversions_2.size}"
       conversions_2.each do |conversion_id| #Same as populate_conversions
-
+        oldCardinality = @currentCardinality
         puts "ID: #{conversion_id}"
         data = ConversionFase2.create(conversion_id: conversion_id, conversion_date: "2020-03-19", user_id: 1)
         Utils::PrestoDb.new.insert_element_presto_fase2(data.id)
@@ -86,38 +96,45 @@ module Api
         if @currentCardinality > oldCardinality  
           AttackVectorFase2.create(number: conversion_id)
         end
-        oldCardinality = @currentCardinality
+        
       end
 
       finish = Time.now
       diff = finish - start
+
+      Utils::Slack::HllBot.send_message("Finished simulation fase 2 at #{finish} with *#{@expectedCardinality}* ", "simulations")
+      Utils::Slack::HllBot.send_message("Took #{diff} seconds to complete. Total size of attack_vector is #{AttackVectorFase2.all.count}", "simulations")
       Execution.create(time: diff.to_s, vector_size: AttackVectorFase2.all.count)
+      Utils::Slack::HllBot.send_message("Cardinality for #{@expectedCardinality} elements is #{@currentCardinality}", "simulations")
+      Utils::Slack::HllBot.send_message("Cardinality for Attack Vector with #{AttackVectorFase2.all.count} elements is #{Utils::PrestoDb.new.get_cardinality_attack_vector("attack_vector_fase2s")}", "simulations")
+      
     end
 
     def fase3(filtered = false) #Fase 3  Insertar de mayor a menor 
       start = Time.now
+
       Utils::Slack::HllBot.send_message("Starting simulation at #{start} with *#{@expectedCardinality}* ", "simulations")
 
       #New HLL | Truncar el antiguo
-      ActiveRecord::Base.connection.execute("Truncate table summary_test ")
+      ActiveRecord::Base.connection.execute("Truncate table summary_test")
 
       conversion_ids = AttackVectorFase2.order(number: :desc) #If one filtering round has been done, get AttackVectorFiltered, where tthe filtered elements are
-      currentCardinality = 0 #Get current cardinality of filtered elements. 0 if null
+      @currentCardinality = 0 #Get current cardinality of filtered elements. 0 if null
       oldCardinality = 0
 
       #Meter los elementos en order inverso
       #Si aumenta cardinalidad, guardar en vector de ataque final
 
       conversion_ids.each do |conversion_id|
-        oldCardinality = currentCardinality
+        oldCardinality = @currentCardinality
 
         puts "ID: #{conversion_id.number}"
         data = ConversionFiltered.create(conversion_id: conversion_id.number, conversion_date: "2020-06-13", user_id: 1) #Insert new element 
 
-        currentCardinality = Utils::PrestoDb.new.get_cardinality_filtered(data.id) #Get cardinality of filtered set 
+        @currentCardinality = Utils::PrestoDb.new.get_cardinality_filtered(data.id) #Get cardinality of filtered set 
 
-        if currentCardinality > oldCardinality  #If increases, insert in attack vector
-          AttackVectorFiltered.where(number: conversion_id.number).first_or_create
+        if@currentCardinality > oldCardinality  #If increases, insert in attack vector
+          AttackVectorFiltered.create(number: conversion_id.number)
         # else #Else, delete it 
         #   AttackVectorFiltered.find_by(number: conversion_id.number).delete if filtered
         end
@@ -125,13 +142,18 @@ module Api
 
       finish = Time.now
       diff = finish - start
-      Utils::Slack::HllBot.send_message("Finished filtering at #{finish} with *#{conversion_ids.count}* ", "simulations")
-      Utils::Slack::HllBot.send_message("Took #{diff} seconds to complete. Total size of attack_vector is #{AttackVectorFiltered.all.count}", "simulations")
+      Utils::Slack::HllBot.send_message("Finished simulation fase 2 at #{finish} with *#{@expectedCardinality}* ", "simulations")
+      Utils::Slack::HllBot.send_message("Took #{diff} seconds to complete. Total size of attack_vector is #{AttackVectorFase2.all.count}", "simulations")
       Execution.create(time: diff.to_s, vector_size: AttackVectorFiltered.all.count)
+      Utils::Slack::HllBot.send_message("Cardinality for #{@expectedCardinality} elements is #{@currentCardinality}", "simulations")
+      Utils::Slack::HllBot.send_message("Cardinality for Attack Vector with #{AttackVectorFiltered.all.count} elements is #{Utils::PrestoDb.new.get_cardinality_attack_vector("attack_vector_filtereds")}", "simulations")
+      
     end
 
     def all
-      
+      reset
+      fase1
+      fase2
       fase3
     end
 
--- lib/utils/presto_db.rb
+++ lib/utils/presto_db.rb
@@ -45,12 +45,12 @@ module Utils
       rows[0][0]
     end
 
-    def get_cardinality_attack_vector
+    def get_cardinality_attack_vector(database)
       # run a query and get results as an array of arrays:
       #ActiveRecord::Base.connection.execute("Truncate table summary_test")
       #Building cardinality
-      
-      @client.run("insert into summary_conversions select cast(approx_set(number) as varbinary) as conversion_hll_sketch from attack_vectors ")
+      ActiveRecord::Base.connection.execute("Truncate table summary_conversions ")
+      @client.run("insert into summary_conversions select cast(approx_set(number) as varbinary) as conversion_hll_sketch from #{database} ")
       #Get cardinality
       columns, rows = @client.run("select cardinality(merge(cast(hll as HyperLogLog))) as daily_conversions from summary_conversions")
       #Return cardinality
@@ -59,40 +59,35 @@ module Utils
     end
 
     def normal_order(database)
-      range = 20000
-      while range < 100001 do
         ActiveRecord::Base.connection.execute("Truncate table summary_conversions ")
-        @client.run("insert into summary_conversions select cast(approx_set(number) as varbinary) as conversion_hll_sketch from #{database}  where number < #{range} group by number")
+        @client.run("insert into summary_conversions select cast(approx_set(number) as varbinary) as conversion_hll_sketch from #{database} group by number")
         columns, rows = @client.run("select cardinality(merge(cast(hll as HyperLogLog))) as daily_conversions from summary_conversions")
         #Return cardinality
-        puts "Cardinality for #{range}: #{rows}"
+        puts "Cardinality for normal_order: #{rows}"
+        puts "Cardinality for normal_order: #{AttackVector.all.count}"
         ActiveRecord::Base.connection.execute("Truncate table summary_conversions ")
-        range += 20000
-      end
     end
 
     def inverted_order(database)
-      range = 20000
-      while range < 100001 do
         ActiveRecord::Base.connection.execute("Truncate table summary_conversions ")
-        @client.run("insert into summary_conversions select cast(approx_set(number) as varbinary) as conversion_hll_sketch from #{database}  where number < #{range} group by number order by number desc")
+        @client.run("insert into summary_conversions select cast(approx_set(number) as varbinary) as conversion_hll_sketch from #{database} group by number order by number desc")
         columns, rows = @client.run("select cardinality(merge(cast(hll as HyperLogLog))) as daily_conversions from summary_conversions")
         #Return cardinality
-        puts "Cardinality for #{range}: #{rows}"
-        range += 20000
-      end
+        puts "Cardinality for inverted_order: #{rows}"
     end
 
     def random_shuffling(database)
-      range = 20000
-      while range < 100001 do
         ActiveRecord::Base.connection.execute("Truncate table summary_conversions ")
-        @client.run("insert into summary_conversions select cast(approx_set(number) as varbinary) as conversion_hll_sketch from #{database}  where number < #{range} group by number order by RAND()")
+        @client.run("insert into summary_conversions select cast(approx_set(number) as varbinary) as conversion_hll_sketch from #{database} group by number order by RAND()")
         columns, rows = @client.run("select cardinality(merge(cast(hll as HyperLogLog))) as daily_conversions from summary_conversions")
         #Return cardinality
-        puts "Cardinality for #{range}: #{rows}"
-        range += 20000
-      end
+        puts "Cardinality for random_shuffling: #{rows}"
+    end
+
+    def get_metrics(database)
+      normal_order(database)
+      inverted_order(database)
+      random_shuffling(database)
     end
   end
 end
